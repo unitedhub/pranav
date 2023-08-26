4a85f5238f16cb803a9c06d964cb6f286a2d383a
unitedhub <psk117777@gmail.com>
2023-08-26 12:23:43 +0530

    java unzipped

---
+++ Operation.java
@@ -0,0 +1,16 @@
+public class Operation {
+    public static void main(String[] args) {
+        int a,b,sum,sub,mul,div;
+        a=10;
+        b=24;
+        sum=a+b;
+        System.out.println("sum is"+ sum);
+        
+        sub=a-b;
+        System.out.println("sub is"+sub);
+        mul=a*b;
+        System.out.println("mul is"+ mul);
+        div=a/b;
+        System.out.println("div is"+div);
+    }
+}
---
+++ Stu.java
@@ -0,0 +1,5 @@
+public class Stu {
+   public static void main(String[] args) {
+    System.out.println("firbolt");
+   } 
+}
